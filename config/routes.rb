06d86c448c3cfde4c1b898c686eb72f61c2ae561
Mark Mulder <markmulder@gmail.com>
2009-09-13 12:26:47 +0200

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+ActionController::Routing::Routes.draw do |map|
+
+  map.root :controller => 'users'
+
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
+
+
+end
