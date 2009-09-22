6236e07a97df31b8b7ee3f2fbc7d8a45642aabe1
Mark Mulder <markmulder@gmail.com>
2009-09-22 19:01:24 +0200

    Now excludes the current track from the tracks#show tracklisting

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -4,6 +4,7 @@ class TracksController < ApplicationController
     @track = Track.find(params[:id])
     @artist = Artist.find(@track.artist_id)
     @tracks = @artist.tracks.all
+    @tracks.delete(@track) #Removes the current displayed track from the tracklisting
   end
   
   def index
