fd63f9ab28054cba4ff446b7e3e63d7b0df45d97
Mark Mulder <markmulder@gmail.com>
2009-09-26 22:54:24 +0200

    Tracks will now autoplay the following track. Also added in a prevtrack function for later use with previous and next buttons.

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -1,22 +1,46 @@
 $(document).ready(function(){
 
+  function nexttrack(current_track) {
+    var nexttrack = current_track + 1;
+    if (nexttrack < 10) {
+      nexttrack = nexttrack.charAt(1);
+    };
+    var trackname = $("#track-" + nexttrack + " h2").html();
+    var nexttrack_url = $("#track-" + nexttrack + " a.play").attr("href");
+    setsongtitle(trackname);
+    playtrack(nexttrack_url);
+  };
+  
+  function prevtrack(current_track) {
+    var prevtrack = current_track - 1;
+    if (prevtrack < 10) {
+      prevtrack = prevtrack.charAt(1);
+    }
+    var trackname = $("track-" + prevtrack + " h2").html();
+    var prevtrack_url = $("#track-" + prevtrack + " a.play").attr("href");
+    setsongtitle(trackname);
+    playtrack(prevtrack_url);
+  };
+  
+  function setsongtitle(title) {
+    $("#player_song_title").html(title);
+  };
+  
+  function playtrack(trackurl ) {
+    $("#jplayer").setFile(trackurl).play();
+  };
+
   $("a.play").click(function(event) {
     event.preventDefault();
     $("#player").slideDown();
     var trackname = $(this).prev().prev().html();
-    var trackurl = $(this).attr("href")
-    $("#player h3").html(trackname);
-    
-    $("#jplayer").setFile(trackurl).play();
-
+    var trackurl = $(this).attr("href");
+    $("#player_song_title").html(trackname);    
+    playtrack(trackurl);
   });
-  
-  
 
   $("#jplayer").jPlayer({
     ready: function () {
-                // $(this).setFile("assault.mp3");
-                $("#player_song_title").html("DJ Assault - Ass and Titties");
     },
     volume: 50,
     oggSupport: false
@@ -34,6 +58,7 @@ $(document).ready(function(){
 
   })
   .onSoundComplete( function() {
-    $(this).play();
+    var current_track = $("#player_song_title a").attr("id");
+    nexttrack(current_track);
   });
 });
