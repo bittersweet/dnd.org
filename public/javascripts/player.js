c65aaeb530f017cdf887430fedb7e0425aa70f89
Mark Mulder <markmulder@gmail.com>
2009-09-26 19:18:13 +0200

    Moved the player js and css into seperate files.

---
+++ public/javascripts/player.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+  $("a.play").click(function(event) {
+    event.preventDefault();
+    $("#player").slideDown();
+    var track = $(this).prev().prev().html();
+    $("#player h3").html(track);
+
+  });
+
+  $("#jplayer").jPlayer({
+    ready: function () {
+                $(this).setFile("assault.mp3").play();
+                $("#song_title").html("DJ Assault - Ass and Titties");
+    },
+    height: 100,
+    volume: 50,
+    oggSupport: false
+  })
+  .jPlayerId("play", "player_play")
+  .jPlayerId("pause", "player_pause")
+  .jPlayerId("stop", "player_stop")
+  .jPlayerId("loadBar", "player_progress_load_bar")
+  .jPlayerId("playBar", "player_progress_play_bar")
+  .onProgressChange( function(loadPercent, playedPercentRelative, playedPercentAbsolute, playedTime, totalTime) {
+    var myPlayedTime = new Date(playedTime);
+    var ptMin = (myPlayedTime.getUTCMinutes() < 10) ? "0" + myPlayedTime.getUTCMinutes() : myPlayedTime.getUTCMinutes();
+    var ptSec = (myPlayedTime.getUTCSeconds() < 10) ? "0" + myPlayedTime.getUTCSeconds() : myPlayedTime.getUTCSeconds();
+    $("#play_time").text(ptMin+":"+ptSec);
+
+  })
+  .onSoundComplete( function() {
+    $(this).play();
+  });
+});
