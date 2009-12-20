732b75f63f0751092fffbd880e6d54c33607474b
Mark Mulder <markmulder@gmail.com>
2009-12-20 15:33:06 +0100

    Make the artist div's clickable.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,4 +4,10 @@ $(function(){
     $("#flash_notice").slideUp();
   }, 2000);
 
+  $("li.artist").click(function() {
+    var url = $("a", this).attr("href");
+    console.log(url);
+    location.href=url;
+  })
+
 });
