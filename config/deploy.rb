38a9e0f992a32b4966f2efa69bab8fb1cc491c17
Mark Mulder <markmulder@gmail.com>
2010-01-14 16:15:46 +0100

    Capified

---
+++ config/deploy.rb
@@ -0,0 +1,43 @@
+default_run_options[:pty] = true
+set :application, 'denachtdienst.org'
+set :user,        'deploy'
+set :port,        30000
+set :repository,   'git@github.com:bittersweet/dnd.org.git'
+set :scm,         'git'
+set :deploy_to,   "/home/deploy/test/#{application}"
+
+ssh_options[:forward_agent] = true
+ssh_options[:username] = 'deploy'
+set :use_sudo,    false
+
+role :app, application
+role :web, application
+role :db, application, :primary => true
+
+namespace :deploy do
+
+  after "deploy:update_code", "deploy:link_production_db", "deploy:link_audio", "deploy:link_app_config"
+
+
+  desc "Link in the production database.yml"
+  task :link_production_db do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+
+  desc "Link audio"
+  task :link_audio do
+    run "ln -nfs #{deploy_to}/shared/audio #{release_path}/public/audio"
+  end
+
+  desc "Link the app_config file"
+  task :link_app_config do
+    run "ln -nfs #{deploy_to}/shared/config/config.yml #{release_path}/config/config.yml"
+  end
+
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+end
