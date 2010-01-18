e07a593fd11eb5a360dfb833682ccba61befeb91
Mark Mulder <markmulder@gmail.com>
2010-01-18 15:31:04 +0100

    No longer using x_sendfile

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,12 +1,12 @@
 class TracksController < ApplicationController
-  
+
   def show
     @track = Track.find(params[:id])
     @artist = Artist.find(@track.artist_id)
     @tracks = @artist.tracks.all
     @tracks.delete(@track) #Removes the current displayed track from the tracklisting
   end
-  
+
   def index
     @tracks = Track.all
   end
@@ -15,7 +15,6 @@ class TracksController < ApplicationController
     @track = Track.find(params[:track_id])
     @track.update_playcount(request.env)
     send_file "#{RAILS_ROOT}/public#{@track.mp3.url}", :type => 'audio/mpeg',
-                                                       :x_sendfile => Rails.env.production?
   end
 
 end
