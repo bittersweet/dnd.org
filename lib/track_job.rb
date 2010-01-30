19a0df6699144762a61da30f43a6123ba73fb536
Mark Mulder <markmulder@gmail.com>
2010-01-30 15:45:40 +0100

    Track name instead of title

--- lib/track_job.rb
+++ lib/track_job.rb
@@ -1,6 +1,6 @@
 class TrackJob < Struct.new(:track_id)
   def perform
     track = Track.find(track_id)
-    Twitterupdate::update("New track: #{track.title} http://denachtdienst.org/tracks/#{track.to_param}")
+    Twitterupdate::update("New track: #{track.name} http://denachtdienst.org/tracks/#{track.to_param}")
   end
 end
