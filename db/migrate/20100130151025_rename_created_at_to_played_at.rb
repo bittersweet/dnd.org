97b0b66ec4f14c860e7b12cf015d6de9596f9a49
Mark Mulder <markmulder@gmail.com>
2010-03-09 15:53:50 +0100

    Get rid of unused migration

--- db/migrate/20100130151025_rename_created_at_to_played_at.rb
+++
@@ -1,9 +0,0 @@
-class RenameCreatedAtToPlayedAt < ActiveRecord::Migration
-  def self.up
-    rename_column :statistics, :created_at, :played_at
-  end
-
-  def self.down
-    rename_column :tracks, :played_at, :created_at
-  end
-end
