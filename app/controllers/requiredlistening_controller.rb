5b529d9d5b3e9ed09e635896ebb872c92ee94233
Mark Mulder <markmulder@gmail.com>
2010-05-01 21:20:14 +0200

    Add requiredlistening#index.

--- app/controllers/requiredlistening_controller.rb
+++ app/controllers/requiredlistening_controller.rb
@@ -1,5 +1,9 @@
 class RequiredlisteningController < ApplicationController
 
+  def index
+    @artists = Artist.requiredlistening
+  end
+
   def show
     @artist = Artist.find(params[:id])
     @tracks = @artist.tracks.required_listening
