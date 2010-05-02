852efec3461195bb10fcb96ce9e95c4377c82775
Mark Mulder <markmulder@gmail.com>
2010-05-02 11:01:43 +0200

    Disable requiring an Avatar for now.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -13,7 +13,8 @@ class Artist < ActiveRecord::Base
                     :convert_options => {:all => "-strip"},
                     :path => ":rails_root/public/artists/:id/:style/:filename",
                     :url => "/artists/:id/:style/:filename"
-  validates_attachment_presence :avatar, :message => "Don't forget to include an image"
+
+  # validates_attachment_presence :avatar, :message => "Don't forget to include an image"
   # validates_attachment_content_type :avatar, :content_type => ['audio/mpeg', 'audio/mpg', 'audio/mp3'], :message => "Please only select audio files"
 
   default_scope :order => 'name ASC'
