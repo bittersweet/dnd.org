d1f5e7aea306dd77213df3292f5708a28cc86d20
Mark Mulder <markmulder@gmail.com>
2010-05-04 23:40:38 +0200

    Do RL section for iPhone.

--- app/controllers/requiredlistening_controller.rb
+++ app/controllers/requiredlistening_controller.rb
@@ -2,11 +2,23 @@ class RequiredlisteningController < ApplicationController
 
   def index
     @artists = Artist.requiredlistening
+
+    respond_to do |format|
+      format.mobile do
+        render :layout => 'application'
+      end
+    end
   end
 
   def show
     @artist = Artist.find(params[:id])
     @tracks = @artist.tracks.required_listening
+
+    respond_to do |format|
+      format.mobile do
+        render :layout => 'application'
+      end
+    end
   end
 
 end
