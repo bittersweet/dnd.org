6db053c4aa815463e44ee8184b1729bed5bda4f1
Mark Mulder <markmulder@gmail.com>
2010-05-04 22:48:01 +0200

    Add work for mobile version.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   helper :all
   filter_parameter_logging :password, :password_confirmation
   protect_from_forgery
+  before_filter :prepare_for_mobile
 
-
- layout :layout_by_resource
+  layout :layout_by_resource
 
   def layout_by_resource
     if devise_controller?
@@ -14,5 +14,15 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  private
+
+  helper_method :mobile_device?
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS/
+  end
+
+  def prepare_for_mobile
+    request.format = :mobile if mobile_device?
+  end
 end
 
