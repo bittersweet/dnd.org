d35e545575b185fbb63d010104c034315ef21f00
Mark Mulder <markmulder@gmail.com>
2010-07-18 17:37:55 +0200

    Disable Machinist object caching for now.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,4 +29,8 @@ DenachtdienstOrg::Application.configure do
   # This is necessary if your schema can't be completely dumped by the schema dumper,
   # like if you have constraints or database-specific column types
   # config.active_record.schema_format = :sql
+
+  Machinist.configure do |config|
+    config.cache_objects = false
+  end
 end
