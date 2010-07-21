37242cbf8ebdd17822dcc22390daeca1eebb8c67
Mark Mulder <markmulder@gmail.com>
2010-07-21 21:30:07 +0200

    Clean up send_file.

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -14,13 +14,8 @@ class TracksController < ApplicationController
     render :nothing => true
     @track = Track.find(params[:id])
     @track.update_playcount(request.env)
-    if Rails.env.production?
-      head(:x_accel_redirect => @track.mp3.url,
-           :content_type => @track.mp3.content_type,
-           :content_disposition => "attachment; filename=\"#{@track.mp3_file_name}\"")
-    else
-      send_file "#{Rails.root}/public#{@track.mp3.url}", :type => @track.mp3.content_type
-    end
+
+    send_file @track.mp3.path, :type => @track.mp3.content_type, :disposition => 'attachment'
   end
 
 end
