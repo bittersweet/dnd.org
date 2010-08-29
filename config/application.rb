96136c68587ba671602e75703d1ef0818b1527f8
Mark Mulder <markmulder@gmail.com>
2010-08-29 14:57:20 +0200

    Add lib to autoload_paths.

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module DenachtdienstOrg
     # Add additional load paths for your own custom dirs
     # config.load_paths += %W( #{config.root}/extras )
 
+    # Rails RC does not autoload files in lib
+    config.autoload_paths += %W( #{config.root}/lib )
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
